4fee78bf81aa4acf4b5f0be6ad5fc605edb9e0ad
Venu Thota <venu.t@comakeit.com>
2023-01-03 13:58:23 +0530

    Added spa test cases

--- src/main/java/dataModel/SPA/Vehicle.java
+++ src/main/java/dataModel/SPA/Vehicle.java
@@ -17,72 +17,65 @@ public class Vehicle implements Serializable {
 	private String ccNumber;
 	private String expiry;
 	private String cvc;
-
+	private String amount;
 	public String getLicensePlateNumber() {
 		return licensePlateNumber;
 	}
-
 	public void setLicensePlateNumber(String licensePlateNumber) {
 		this.licensePlateNumber = licensePlateNumber;
 	}
-
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
 	public String getZip() {
 		return zip;
 	}
-
 	public void setZip(String zip) {
 		this.zip = zip;
 	}
-
 	public String getLocationNumber() {
 		return locationNumber;
 	}
-
 	public void setLocationNumber(String locationNumber) {
 		this.locationNumber = locationNumber;
 	}
-
 	public String getCcNumber() {
 		return ccNumber;
 	}
-
 	public void setCcNumber(String ccNumber) {
 		this.ccNumber = ccNumber;
 	}
-
 	public String getExpiry() {
 		return expiry;
 	}
-
 	public void setExpiry(String expiry) {
 		this.expiry = expiry;
 	}
-
 	public String getCvc() {
 		return cvc;
 	}
-
 	public void setCvc(String cvc) {
 		this.cvc = cvc;
 	}
-
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
 	@Override
 	public String toString() {
 		return "Vehicle [licensePlateNumber=" + licensePlateNumber + ", state=" + state + ", zip=" + zip
 				+ ", locationNumber=" + locationNumber + ", ccNumber=" + ccNumber + ", expiry=" + expiry + ", cvc="
-				+ cvc + "]";
+				+ cvc + ", amount=" + amount + "]";
 	}
+
+	
 
 }
--- src/test/java/testcases/SPA/Sessions_Testcases.java
+++ src/test/java/testcases/SPA/Sessions_Testcases.java
@@ -22,22 +22,24 @@ public class Sessions_Testcases extends BaseClass {
 	SPA_LocationPage locationPage = new SPA_LocationPage();
 
 	// ****************** TEST SCRIPTS ****************************//
-	
-/*
- * TC_01_Purchase_Session_With_NewCard_and_NewVehicle
- * TC_02_Purchase_Session_With_NewCard_and_ExistingVehicle
- * TC_03_Purchase_Session_With_ExistingCard_and_NewVehicle
- * TC_04_Purchase_Session_With_ExistingCard_and_ExistingVehicle
- */
-	
+
 	/*
-	 * This is a test case to purchase session
+	 * TC_01_Purchase_Session_With_NewCard_and_NewVehicle
+	 * TC_02_Purchase_Session_With_NewCard_and_ExistingVehicle
+	 * TC_03_Purchase_Session_With_ExistingCard_and_NewVehicle
+	 * TC_04_Purchase_Session_With_ExistingCard_and_ExistingVehicle
+	 */
+
+	/*
+	 * This is a test case to purchase session with new vehicle and new payment
+	 * method
 	 *
 	 * Author : Pavan Prasad(pavanprasad.v@comakeit.com)
 	 */
 	@Test(groups = { "smoke", "regression" })
-	public void TC_01_Purchase_Session_With_NewCard() {
-		Boolean newCard = true;
+	public void TC_01_Purchase_Session_With_NewCard_and_NewVehicle() {
+		Boolean newCard = true, newVehicle = true, newSession = true;
+
 		spaLoginPage = launch_SPA_Application();
 		accountsPage = spaLoginPage.login();
 		spaHomePage = accountsPage.navigateToHomePage();
@@ -45,7 +47,28 @@ public class Sessions_Testcases extends BaseClass {
 		vehicle.setLicensePlateNumber(getRandomLicencePlate());
 		locationPage = spaHomePage.navigate_To_LocationPage(vehicle.getLocationNumber());
 
-		locationPage.purchase_Session(vehicle, newCard);
+		locationPage.purchase_Session(vehicle, newVehicle, newCard);
+		locationPage.verify_Purchase_Details(vehicle, newSession);
+	}
+
+	/*
+	 * This is a test case to purchase session with new vehicle and new payment
+	 * method
+	 *
+	 * Author : Pavan Prasad(pavanprasad.v@comakeit.com)
+	 */
+	@Test(groups = { "smoke", "regression" })
+	public void TC_02_Purchase_Session_With_NewCard_and_ExistingVehicle() {
+		Boolean newCard = true, newVehicle = false, newSession = true;
+		spaLoginPage = launch_SPA_Application();
+		accountsPage = spaLoginPage.login();
+		spaHomePage = accountsPage.navigateToHomePage();
+		Vehicle vehicle = xml_Ops.getVehicleTestData();
+		vehicle.setLicensePlateNumber("TS65HJ");
+		locationPage = spaHomePage.navigate_To_LocationPage(vehicle.getLocationNumber());
+
+		locationPage.purchase_Session(vehicle, newVehicle, newCard);
+		locationPage.verify_Purchase_Details(vehicle, newSession);
 	}
 
 }
