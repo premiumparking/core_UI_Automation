ec8304fe59b0f1c720bee73620fd0e8f3899f65e
KVS SIVA KUMAR <sivakumar.kurnool@xebia.com>
2024-02-09 10:34:40 +0530

    Changes to textpay workflow automation

--- send_email_sparkpost.py
+++ send_email_sparkpost.py
@@ -11,7 +11,7 @@ date_str = os.getenv('DATE_STR')
 
 # SparkPost SMTP configuration
 smtp_host = os.getenv('SPARKPOST_SMTP_HOST')
-smtp_port = int(os.getenv('SPARKPOST_SMTP_PORT'))
+smtp_port = os.getenv('SPARKPOST_SMTP_PORT')
 smtp_username = os.getenv('SPARKPOST_SMTP_USERNAME')
 smtp_password = os.getenv('SPARKPOST_API_KEY')
 
@@ -25,7 +25,7 @@ body = f"The textpay automation test report for {date_str} is attached."
 # Create a multipart message
 msg = MIMEMultipart()
 msg['From'] = from_email
-msg['To'] = ", ".join(to_emails)
+msg['To'] = to_emails
 msg['Subject'] = subject
 
 # Attach the email body
@@ -36,28 +36,21 @@ filename = f"test-report-{date_str}.zip"
 attachment_path = f"./test-report-{date_str}.zip"
 
 
-# Open the file in binary mode
-with open(attachment_path, 'rb') as attachment:
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header(
-        'Content-Disposition',
-        f'attachment; filename= {filename}',
-    )
-    msg.attach(part)
-
-print("SMTP Host:", smtp_host)  # should not be None
-print("SMTP Port:", smtp_port)  # should not be None
-print("SMTP Username:", smtp_username)  # should not be None
-print("SMTP Password:", smtp_password)  # should not be None
+try:
+    # Attempt to establish an SMTP connection
+    server = smtplib.SMTP(smtp_host, int(smtp_port))
+    server.set_debuglevel(1)  # Enable debug output to see the connection details
+    server.starttls()  # Upgrade the connection to secure
+    server.login(smtp_username, smtp_password)  # Try to log in to the server
+    print("SMTP connection and authentication successful.")
+except smtplib.SMTPException as e:
+    print(f"SMTP error occurred: {e}")
+    exit(1)
+except Exception as e:
+    print(f"An error occurred: {e}")
+    exit(1)
 
 
-# Send the email over SMTP
-server = smtplib.SMTP(smtp_host, smtp_port)
-server.starttls()
-server.login(smtp_username, smtp_password)
-server.sendmail(from_email, to_emails, msg.as_string())
 server.quit()
 
 print('Email sent successfully.')
